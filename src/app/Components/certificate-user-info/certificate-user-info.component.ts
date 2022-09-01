49ece80f3f7fbf31691dad54d9055de24edf511d
BelieverInGod <Stepanko919@gmail.com>
2022-09-01 03:37:16 +0300

    ts.config

--- src/app/Components/certificate-user-info/certificate-user-info.component.ts
+++ src/app/Components/certificate-user-info/certificate-user-info.component.ts
@@ -11,17 +11,38 @@ export class CertificateUserInfoComponent implements OnInit {
   @Input() users: Users[] = []
   @Input() flagActiveInfo:number = 0
   @Input() currentId:number = 0
+  ASN1 = require('@lapo/asn1js');
+  Hex = require('@lapo/asn1js/hex');
+  file:any
+  result:any
+  baseString:any
   
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  file:any
   getFile(event:any) {
     this.file = event.target.files[0]
     console.log(this.file)
+    this.baseString = this.getBase64(event.target.files[0])
+
+    this.result = this.ASN1.decode(this.Hex.decode(this.baseString)).content();
+    console.log(this.result)
   }
+
+  getBase64(file:any) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
+
+ 
+  
 
 }
